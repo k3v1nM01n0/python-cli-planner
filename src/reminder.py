ab14d8c4604382f459457c9f7c50cb21f09012c6
kevindson <kevinmoino10@gmail.com>
2022-02-06 13:03:30 +0300

    f/template

---
+++ src/reminder.py
@@ -0,0 +1,12 @@
+class PrefixedReminder:
+    """This class acts as a base class for other types of reminders.
+    Classes that subclass it should override the `self.text` property
+    """
+    def __init__(self, prefix="Hey, don't forget to "):
+        self.prefix = prefix
+        self.text = prefix + '<placeholder_text>'
+
+
+class PoliteReminder(PrefixedReminder):
+    def __init__(self) -> None:
+        PrefixedReminder.__init__()
